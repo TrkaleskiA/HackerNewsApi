092a80f2b7148b4f43dd6b48a756abb01395b7ea
BojkovaA <108904427+BojkovaA@users.noreply.github.com>
2024-08-07 15:26:30 +0200

    Get User by Id

---
+++ HackerNewsApi/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HackerNews.DataAccess;
+using HackerNews.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace HackerNewsApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+
+            _context = context; 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+    }
+}
