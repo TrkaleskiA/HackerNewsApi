d46d2cdcf2274b342c1c4a8dd1b5584d00fdb6fb
BojkovaA <108904427+BojkovaA@users.noreply.github.com>
2024-08-08 12:37:58 +0200

    Implement Login

--- HackerNewsApi/Controllers/UserController.cs
+++ HackerNewsApi/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using HackerNews.DataAccess;
 using HackerNews.DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using HackerNewsApi.Model;
 
 namespace HackerNewsApi.Controllers
 {
@@ -40,5 +41,22 @@ namespace HackerNewsApi.Controllers
 
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login([FromBody] LoginModel loginModel)
+        {
+            // Find the user by username
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginModel.Username);
+
+            if (user == null || user.Password != loginModel.Password)
+            {
+                return Unauthorized("Invalid username or password");
+            }
+
+            // Return the user information (excluding the password)
+            return Ok(new { user.Id, user.Username, user.Nickname, user.IsAdmin });
+        }
+
     }
+
+    
 }
