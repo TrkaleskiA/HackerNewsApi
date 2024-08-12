9da805ce4a89cfae9b5054de92c5c057d92cf5f1
Aleksandar Trkaleski <a.trkaleski@gmail.com>
2024-08-12 12:29:24 +0200

    Header styling and bootstrap implementation changes.

--- hackernewsreact/src/App.tsx
+++ hackernewsreact/src/App.tsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Components/Register';
 import HackerNews from './Components/Hackernews/HackerNews';
