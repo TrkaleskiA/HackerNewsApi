e949d2c6b297602d053320d8278600895f414c00
BojkovaA <108904427+BojkovaA@users.noreply.github.com>
2024-08-12 14:32:36 +0200

    edit sidebar

--- hackernewsreact/src/Components/Hackernews/Sidebar.tsx
+++ hackernewsreact/src/Components/Hackernews/Sidebar.tsx
@@ -1,32 +1,32 @@
 import './Sidebar.css'
 const Sidebar = () => {
-    return (  
-            
-                <ul className="custom-list">
-                    <div className="custom-list-div active" id="all">
-                        <li className="news"><a href="#">All</a></li>
-                    </div>
-                    <div className="custom-list-div" id="hot">
-                        <li className="hot"><a href="#">Hot</a></li>
-                    </div>
-                    <div className="custom-list-div" id="show-hn">
-                        <li className="show-hn"><a href="#">Show HN</a></li>
-                    </div>
-                    <div className="custom-list-div" id="ask-hn">
-                        <li className="ask-hn"><a href="#">Ask HN</a></li>
-                    </div>
-                    <div className="custom-list-div" id="poll">
-                        <li className="polls"><a href="#">Polls</a></li>
-                    </div>
-                    <div className="custom-list-div" id="job">
-                        <li className="jobs"><a href="#">Jobs</a></li>
-                    </div>
-                    <div className="custom-list-div" id="starred">
-                        <li className="starred"><a href="#">Starred</a></li>
-                    </div>
-                </ul>
-           
-            
+    return (
+
+        <ul className="custom-list">
+            <div className="custom-list-div active" id="all">
+                <li className="news"><a href="#">All</a></li>
+            </div>
+            <div className="custom-list-div" id="hot">
+                <li className="hot"><a href="#">Hot</a></li>
+            </div>
+            <div className="custom-list-div" id="show-hn">
+                <li className="show-hn"><a href="#">Show HN</a></li>
+            </div>
+            <div className="custom-list-div" id="ask-hn">
+                <li className="ask-hn"><a href="#">Ask HN</a></li>
+            </div>
+            <div className="custom-list-div" id="poll">
+                <li className="polls"><a href="#">Polls</a></li>
+            </div>
+            <div className="custom-list-div" id="job">
+                <li className="jobs"><a href="#">Jobs</a></li>
+            </div>
+            <div className="custom-list-div" id="starred">
+                <li id="starred-link"><a href="#">Starred</a></li>
+            </div>
+        </ul>
+
+
     );
 };
 
