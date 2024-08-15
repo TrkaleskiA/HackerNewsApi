262b78745b6e9db3f237a88ce9db948a3c1b8e34
BojkovaA <108904427+BojkovaA@users.noreply.github.com>
2024-08-15 15:02:20 +0200

    Edit SideBar

--- hackernewsreact/src/Components/Hackernews/Sidebar.tsx
+++ hackernewsreact/src/Components/Hackernews/Sidebar.tsx
@@ -1,32 +1,42 @@
-import './Sidebar.css'
-const Sidebar = () => {
-    return (
+import { FC } from 'react';
+import './Sidebar.css';
 
+// Define the type for the filter options
+type FilterType = 'all' | 'hot' | 'show-hn' | 'ask-hn' | 'poll' | 'job' | 'starred';
+
+interface SidebarProps {
+    onFilterChange: (filterType: FilterType) => void;
+}
+
+const Sidebar: FC<SidebarProps> = ({ onFilterChange }) => {
+    const handleFilterClick = (filterType: FilterType) => {
+        onFilterChange(filterType);
+    };
+
+    return (
         <ul className="custom-list">
-            <div className="custom-list-div active" id="all">
+            <div className="custom-list-div" id="all" onClick={() => handleFilterClick('all')}>
                 <li className="news"><a href="#">All</a></li>
             </div>
-            <div className="custom-list-div" id="hot">
+            <div className="custom-list-div" id="hot" onClick={() => handleFilterClick('hot')}>
                 <li className="hot"><a href="#">Hot</a></li>
             </div>
-            <div className="custom-list-div" id="show-hn">
+            <div className="custom-list-div" id="show-hn" onClick={() => handleFilterClick('show-hn')}>
                 <li className="show-hn"><a href="#">Show HN</a></li>
             </div>
-            <div className="custom-list-div" id="ask-hn">
+            <div className="custom-list-div" id="ask-hn" onClick={() => handleFilterClick('ask-hn')}>
                 <li className="ask-hn"><a href="#">Ask HN</a></li>
             </div>
-            <div className="custom-list-div" id="poll">
+            <div className="custom-list-div" id="poll" onClick={() => handleFilterClick('poll')}>
                 <li className="polls"><a href="#">Polls</a></li>
             </div>
-            <div className="custom-list-div" id="job">
+            <div className="custom-list-div" id="job" onClick={() => handleFilterClick('job')}>
                 <li className="jobs"><a href="#">Jobs</a></li>
             </div>
-            <div className="custom-list-div" id="starred">
+            <div className="custom-list-div" id="starred" onClick={() => handleFilterClick('starred')}>
                 <li id="starred-link"><a href="#">Starred</a></li>
             </div>
         </ul>
-
-
     );
 };
 
